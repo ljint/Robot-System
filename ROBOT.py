7f0fc1ad2e3f85ee031e447604e4235d3f71bc2a
ljint <122286065@qq.com>
2019-07-21 14:32:05 +1000

    Redo
    use simple way to done

--- ROBOT.py
+++ ROBOT.py
@@ -3,27 +3,58 @@ import sys
 def move(command):
   x=0
   y=0
-  a=1
-  b=1
+  a=0
+  b=0
+  facing=0 # 0="north", 1="south", 2="east", 3="west"
   for step in command.split(','):
     order = step[0]
     num = int(step[1:])
     if order == 'F':
       while num > 0:
-        if a == 1:
-          y = y + a * b
-        else:
-          x = x - a * b
+        if facing == 0:
+          b = b + 1
+        if facing == 1:
+          b = b - 1
+        if facing == 2:
+          a = a + 1
+        if facing == 3:
+          a = a - 1
         num = num -1
     if order == 'B':
       while num > 0:
-        if a == 1:
-          y = y - a * b
-        else:
-          x = x + a * b
+        if facing == 0:
+          b = b - 1
+        if facing == 1:
+          b = b + 1
+        if facing == 2:
+          a = a - 1
+        if facing == 3:
+          a = a + 1
         num = num -1
-
-  print ('(',x,',',y,')')
-  print (abs(x)+abs(y))
+    if order == 'R':
+      while num > 0:
+        if facing == 0:
+          facing = 2
+        elif facing == 1:
+          facing = 3
+        elif facing == 2:
+          facing = 1
+        elif facing == 3:
+          facing = 0
+        num = num -1
+    if order == 'L':
+      while num > 0:
+        if facing == 0:
+          facing = 3
+        elif facing == 1:
+          facing = 2
+        elif facing == 2:
+          facing = 0
+        elif facing == 3:
+          facing = 1
+        num = num -1
+        
+  print ('(',a,',',b,')')
+  print (abs(a-x)+abs(b-y))
    
 move(sys.argv[1])
